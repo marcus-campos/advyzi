ea8fe041cf9bc258a590e920d98b1a8fa6e86bf9
Marcus Campos <marcus_ultimate@hotmail.com>
2016-12-09 11:21:40 -0200

    v.1.5.1 - green

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -41,7 +41,7 @@
 
             <div class="col-md-4 col-sm-6 col-xs-12">
                 <div class="info-box">
-                    <span class="info-box-icon bg-red"><i class="fa fa-users"></i></span>
+                    <span class="info-box-icon bg-green"><i class="fa fa-users"></i></span>
 
                     <div class="info-box-content">
                         <span class="info-box-text text-center">Clientes adicionados</span>
